9be573c0f34421d183c17b0bbb5144c7ba142264
Ethan Nichols <ethan.thomas.nichols@gmail.com>
2018-04-14 22:42:57 -0400

    Base files for the Metldown Project

---
+++ AppClass.cpp
@@ -0,0 +1,62 @@
+#include "AppClass.h"
+using namespace Simplex;
+void Application::InitVariables(void)
+{
+	//Set the position and target of the camera
+	m_pCameraMngr->SetPositionTargetAndUp(
+		vector3(0.0f, 0.0f, 13.0f), //Position
+		vector3(0.0f, 0.0f, 12.0f),	//Target
+		AXIS_Y);					//Up
+
+	m_pLightMngr->SetPosition(vector3(0.0f, 3.0f, 13.0f), 1); //set the position of first light (0 is reserved for ambient light)
+	
+	std::cout << "Hello" << std::endl;
+
+	//Entity Manager
+	m_pEntityMngr = MyEntityManager::GetInstance();
+}
+void Application::Update(void)
+{
+	//Update the system so it knows how much time has passed since the last call
+	m_pSystem->Update();
+
+	//Is the ArcBall active?
+	ArcBall();
+
+	//Is the first person camera active?
+	CameraRotation();
+
+	//Update Entity Manager
+	m_pEntityMngr->Update();
+		
+	//Add objects to render list
+	m_pEntityMngr->AddEntityToRenderList(-1, true);
+}
+void Application::Display(void)
+{
+	// Clear the screen
+	ClearScreen();
+	
+	// draw a skybox
+	m_pMeshMngr->AddSkyboxToRenderList();
+	
+	//render list call
+	m_uRenderCallCount = m_pMeshMngr->Render();
+
+	//clear the render list
+	m_pMeshMngr->ClearRenderList();
+	
+	//draw gui
+	DrawGUI();
+	
+	//end the current frame (internally swaps the front and back buffers)
+	m_pWindow->display();
+}
+void Application::Release(void)
+{
+	//release the entity manager
+	m_pEntityMngr->ReleaseInstance();
+
+	//release GUI
+	ShutdownGUI();
+}
