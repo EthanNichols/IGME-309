f9383292adf5997761ecd89e0f8860215184ae21
dgl6789 <dgl6789@IGMOH1370-11>
2018-04-17 12:18:18 -0400

    flipped the ship around properly

--- Player.cpp
+++ Player.cpp
@@ -19,12 +19,12 @@ namespace Player {
 
 		//Create the player
 		p_entityManager->AddEntity("models\\model_ship.obj", "ship");
-		p_entityManager->SetModelMatrix(Simplex::IDENTITY_M4);
+		p_entityManager->SetModelMatrix(glm::rotate(Simplex::IDENTITY_M4, 180.0f, 0.0f, 1.0f, 0.0f));
 	}
 
 	///Return the position of the player
 	Simplex::vector3 GetPosition() {
-		Simplex::vector4 vec4Pos = p_entityManager->GetModelMatrix() * Simplex::vector4(1, 1, 1, 1);
+		Simplex::vector4 vec4Pos = p_entityManager->GetModelMatrix() * Simplex::vector4(0, 0, 0, 1);
 
 		return Simplex::vector3(vec4Pos.x, vec4Pos.y, vec4Pos.z);
 	}
@@ -34,7 +34,7 @@ namespace Player {
 
 		//Move the last entity added slowly to the right
 		Simplex::matrix4 lastMatrix = p_entityManager->GetModelMatrix("ship");// get the model matrix of the last added
-		lastMatrix *= glm::translate(Simplex::IDENTITY_M4, Simplex::vector3(0.0f, 0.0f, -speed)); //translate it
+		lastMatrix *= glm::translate(Simplex::IDENTITY_M4, Simplex::vector3(0.0f, 0.0f, speed)); //translate it
 		p_entityManager->SetModelMatrix(lastMatrix, "ship"); //return it to its owner
 
 	}
